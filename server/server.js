21e70afac7636eb3558aae8f7c4b691fb777b87c
Avishkar Dhanorkar <avidhanorkar23@gmail.com>
2025-01-19 16:57:54 +0530

    updated the frontend URL

--- server/server.js
+++ server/server.js
@@ -9,20 +9,18 @@ configDotenv();
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // Replace with your React app's URL
+    origin: 'https://storygrid-full-stack-project-frontend.onrender.com', // Updated to allow the frontend's URL
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
     credentials: true,
 }
-app.use(cors(
-    corsOptions
-));
+app.use(cors(corsOptions));
 
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
-app.use("/api/v1", routes)
+app.use("/api/v1", routes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
-})
+});
 connectDB();
