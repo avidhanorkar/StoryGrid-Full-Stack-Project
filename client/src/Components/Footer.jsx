e67eb4bc96ccf33dc220d853ade62a5bea9fc273
Avishkar Dhanorkar <136093003+avidhanorkar@users.noreply.github.com>
2025-01-22 09:37:19 +0530

    Update Footer.jsx

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer className="h-16 flex justify-center items-center   ">
         <div className="w-full border-t-2 p-4">
           <span className="block text-sm text-gray-500 text-center">
-            © 2023{" "}StoryGrid. {" "} All Rights Reserved.
+            © 2025{" "}StoryGrid. {" "} All Rights Reserved.
           </span>
         </div>
       </footer>
